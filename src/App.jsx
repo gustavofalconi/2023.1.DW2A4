ed3757e7c2b4c77ad2246f0e8970c22de1a90535
Gustavo <gustavo123755@gmail.com>
2023-06-17 01:59:07 -0300

    Até criação button

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import "./App.css";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+
+const App  = () => {
+  const [tasks, setTasks] = useState([
+    {
+       id: "1",
+       title: "Estudar Programação",
+       completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler Livros",
+      completed: true,
+    },
+  ]);
+  return(
+    <>
+      <div className="container">
+          <AddTask />
+          <Tasks tasks={tasks} />
+      </div>   
+    </>
+  );
+};
+
+export default App;
