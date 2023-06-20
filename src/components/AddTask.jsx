b135b6b2710dc56024f63ede6b52aaf9bfa3fa46
Gustavo <gustavo123755@gmail.com>
2023-06-20 11:34:28 -0300

    Alterações finais

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import "./AddTask.css";
 import Button from './Button';
-import { useState } from 'react';
 
 const AddTask = ({ handleTaskAddition }) => {
-    const[inputData, setInputData] = useState("");
-    
-    const handleInputChange = (e) => {
-      setInputData(e.target.value)
-    }
+  const [inputData, setInputData] = useState("");
 
-    const handleAddTaskClick = () => {
-        handleTaskAddition(inputData);
-        setInputData("");
-    }
+  const handleInputChange = (e) => {
+    setInputData(e.target.value);
+  }
 
-    return (
-        <div className="add-task-container">
-          <input onChange={handleInputChange} value={inputData} className="add-task-input" type="text" />
-          <div className="add-task-button-container"><Button onClick={handleAddTaskClick}>Adicionar</Button></div>
-        </div>
-      );
+  const handleAddTaskClick = () => {
+    if (inputData.trim() !== "") {
+      handleTaskAddition(inputData);
+      setInputData("");
+    } else {
+      alert("O campo não pode estar vazio");
+    }
+  }
+
+  return (
+    <div className="add-task-container">
+      <input onChange={handleInputChange} value={inputData} className="add-task-input" type="text" />
+      <div className="add-task-button-container">
+        <Button onClick={handleAddTaskClick}>Adicionar</Button>
+      </div>
+    </div>
+  );
 }
- 
+
 export default AddTask;
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,35 +1,42 @@
 import React from "react";
-import {CgClose, CgInfo} from "react-icons/cg";
-import {useHistory} from "react-router-dom";
+import { CgClose, CgInfo } from "react-icons/cg";
+import { useHistory } from "react-router-dom";
 
 import "./Task.css";
 
-const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
-    const history = useHistory();
+const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
+  const history = useHistory();
 
-    const handleTaskDetailsClick = () => {
-		history.push(`/${task.title}`);
-	};
+  const handleTaskDetailsClick = () => {
+    history.push(`/${task.title}`);
+  };
 
-    return(
-         <div className="task-container"  style={task.completed ? {borderLeft: "6px solid chartreuse"} : {}}>
+  const confirmTaskDeletion = () => {
+    const confirmMessage = `Tem certeza que deseja excluir a tarefa "${task.title}"?`;
+    if (window.confirm(confirmMessage)) {
+      handleTaskDeletion(task.id);
+    }
+  };
 
-            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
-                {task.title}
-            </div>
+  return (
+    <div className="task-container" style={task.completed ? { borderLeft: "6px solid rgb(66, 141, 255)" } : {}}>
 
-            <div className="buttons-container">
+      <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+        {task.title}
+      </div>
 
-                <button className="remove-task-button" onClick={() => handleTaskDeletion(task.id)}>
-                    <CgClose />
-                </button>
+      <div className="buttons-container">
 
-                <button className="see-task-details-button" onClick={handleTaskDetailsClick}>
-                    <CgInfo />
-                </button>
-            </div>
-        </div>
-    );
+        <button className="remove-task-button" onClick={confirmTaskDeletion}>
+          <CgClose />
+        </button>
+
+        <button className="see-task-details-button" onClick={handleTaskDetailsClick}>
+          <CgInfo />
+        </button>
+      </div>
+    </div>
+  );
 };
- 
+
 export default Task;
